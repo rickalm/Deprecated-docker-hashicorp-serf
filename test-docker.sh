71fd4ce3ad8c128d447a25b61d69c72cbdefa473
CoreOS Admin <core@ip-10-250-5-51.us-west-2.compute.internal>
2016-04-21 23:04:58 +0000

    .

--- test-docker.sh
+++ test-docker.sh
@@ -8,18 +8,16 @@ docker rm -f serf-bridge 2>/dev/null
   --name=serf \
   -e "DEBUG=yes" \
   -e "PORT_SCAN=yes" \
-  -e "PORT_SCAN_MASK=22" \
-  -e "RRPC_IP=${docker_ip}" \
+  -e "PORT_SCAN_MASK=subnet" \
   rickalm/hashicorp-serf \
   && docker exec -it serf /bin/bash
 
 /bin/true && docker run -d \
   --net=bridge \
   --name=serf-bridge \
+  -e "DEBUG=yes" \
   -p 41000:7946 \
   -p 41000:7946/udp \
-  -e "JOIN_LIST=$(hostname -i)" \
-  -e "ADVERTISE_IP=$(hostname -i|awk '{print $1}')" \
   -v /var/run/docker.sock:/var/run/docker.sock \
   rickalm/hashicorp-serf \
   && docker exec -it serf-bridge /bin/bash
